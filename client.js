fbaebea22ca1fb2279a1e874ef786a78b8ae953c
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2020-12-11 16:53:24 -0600

    addEmployeeButton now creates new object, pushes it to the employee array, and refreshes the DOM to add the newly added employee to the DOM.

--- client.js
+++ client.js
@@ -1,13 +1,43 @@
-console.log('js loaded!');
+console.log('js git status!');
 
 $(readyNow);
 
 function readyNow() {
     console.log('jQuery ready');
+
+    renderToDom();
     
-    $('#addEmployee').on('click', addEmployee);
+    $('#addEmployeeButton').on('click', addEmployee);
 }
 
+let employees = [
+    {firstName:'Mister', 
+    lastName:'Fox', 
+    ID:1234, 
+    title:'Trickster', 
+    salary:100000,}
+];
+
+function renderToDom(){
+    //clear display 
+    $('#newEmployee').empty();
+    //use everything in the array, and append them accordingly
+    for(let employee of employees){
+        $('#newEmployee').append(`
+            <tr class="employee">
+             <td>${employee.firstName}</td>
+             <td>${employee.lastName}</td>
+             <td>${employee.ID}</td>
+             <td>${employee.title}</td>
+             <td>$${employee.salary}</td>
+             <td><button id="remove" class="btn btn-danger">Remove</button></td>
+
+             </tr>`)
+    }
+}
+
+
+// Runs when addEmployeeButton is clicked.
 function addEmployee() {
     console.log('Employee added.');
     let firstName = $('#firstNameIn').val();
@@ -20,5 +50,26 @@ function addEmployee() {
     console.log(job);
     let salary = $('#annualSalaryIn').val();
     console.log(salary);
+
+    let newEmployee = {
+        firstName:$('#firstNameIn').val(),
+        lastName:$('#lastNameIn').val(),
+        ID:$('#employeeIDIn').val(),
+        title:$('#jobTitleIn').val(),
+        salary:$('#annualSalaryIn').val(),
+    }
+    console.log(newEmployee);
+    employees.push(newEmployee);
     
+
+    clearInputs();
+    renderToDom();
+}
+
+function clearInputs() {
+    $('#firstNameIn').val('');
+    $('#lastNameIn').val('');
+    $('#employeeIDIn').val('');
+    $('#jobTitleIn').val('');
+    $('#annualSalaryIn').val('');
 }
