28c5e6df42000dabb0b4aee50b20261c570537f3
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2020-12-11 15:06:44 -0600

    each input now logs to the console when Add Employee button is pushed.

--- client.js
+++ client.js
@@ -10,5 +10,15 @@ function readyNow() {
 
 function addEmployee() {
     console.log('Employee added.');
+    let firstName = $('#firstNameIn').val();
+    console.log(firstName);
+    let lastName = $('#lastNameIn').val();
+    console.log(lastName);
+    let employeeID = $('#employeeIDIn').val();
+    console.log(employeeID);
+    let job = $('#jobTitleIn').val();
+    console.log(job);
+    let salary = $('#annualSalaryIn').val();
+    console.log(salary);
     
 }
