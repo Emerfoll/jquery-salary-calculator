dd6103fbcbefd7fe2c2d4bc061009c2a1d053300
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2020-12-11 21:18:15 -0600

    Added comments to explain what each step is doing.

--- client.js
+++ client.js
@@ -18,22 +18,23 @@ let employees = [
         lastName: 'Fox',
         ID: 87425,
         title: 'Trickster',
-        salary: 100000,
+        salary: 80000,
     },
     {
         firstName: 'Bob',
         lastName: 'Loblaw',
         ID: 5068,
         title: 'Trickster',
-        salary: 120000,
+        salary: 110000,
     }
 ];
 
+
 function renderToDom() {
-    //clear display 
+    // Clear display 
     $('#newEmployee').empty();
-    //use everything in the array, and append them accordingly
-    for (let i=0; i<employees.length; i++) {
+    // Loop through array and display to the DOM
+    for (let i = 0; i < employees.length; i++) {
         $('#newEmployee').append(`
             <tr class="employee">
              <td>${employees[i].firstName}</td>
@@ -46,6 +47,7 @@ function renderToDom() {
              </tr>`)
 
     }
+    // Recalculate the Total Monthly Cost.
     calculateMonthlyCost();
 
 }
@@ -65,9 +67,11 @@ function addEmployee() {
     let salary = $('#annualSalaryIn').val();
     console.log(salary);
 
+    // Don't allow entry if they're are empty input fields.
     if (!firstName || !lastName || !employeeID || !job || !salary) {
         console.log('Please fill all boxes');
-    } else {
+    } // If input fields are filled, create an object to push to the employees array.
+    else {
         let newEmployee = {
             firstName: firstName,
             lastName: lastName,
@@ -78,11 +82,14 @@ function addEmployee() {
         console.log(newEmployee);
         employees.push(newEmployee);
 
+        // Clear the input fields.
         clearInputs();
+        // Re-apply what is on the DOM
         renderToDom();
     }
 }
 
+// Clears the input fields.
 function clearInputs() {
     $('#firstNameIn').val('');
     $('#lastNameIn').val('');
@@ -91,35 +98,38 @@ function clearInputs() {
     $('#annualSalaryIn').val('');
 }
 
-// delete and recalculate the Total Monthly Expense.
-
+// Delete and recalculate the Total Monthly Expense.
 function removeEmployee() {
     console.log('Employee Fired');
     $(this).parent().parent().remove();
 
     let value = this.id
 
-    employees.splice(value , 1)
+    employees.splice(value, 1)
     calculateMonthlyCost();
-    
+
 }
 
 
 
 // Calculate Total Monthly Expense.
 function calculateMonthlyCost() {
+    // generate yearly salary per employee
     let monthlyCost = 0;
-
     for (let monthPay of employees) {
         monthlyCost += Number(monthPay.salary);
-    } monthlyCost = monthlyCost / 12;
+    } // Find monthly cost based off yearly salary.
+    monthlyCost = monthlyCost / 12;
     let total = $('#monthlyCost');
+    //Empty current monthly cost.
     total.empty();
+    //Insert new updated monthly cost.
     total.append(Math.ceil(monthlyCost));
-    if ( monthlyCost >= 20000) {
+    //If monthly cost is over 20k, add red background.
+    if (monthlyCost >= 20000) {
         $('#monthlyCost').addClass("overBudget")
     } else if (monthlyCost < 20000) {
         $('#monthlyCost').removeClass('overBudget')
     }
-    console.log('monthlyCost');
+    console.log(monthlyCost);
 }
